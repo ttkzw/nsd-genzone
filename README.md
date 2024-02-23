db16028b2d47740441b381995812d8d6274a063d
Takashi Takizawa <taki@cyber.email.ne.jp>
2024-02-23 20:05:21 +0900

    first commit

--- README.md
+++ README.md
@@ -5,28 +5,29 @@ This is a project to generate test zones for NSD.
 ## Usage
 
 ```
-usage: nsdgenzone [-h] --zones ZONES --hosts HOSTS --domainname DOMAINNAME 
-                  --ipaddress IPADDRESS --configdir CONFIGDIR --zonesdir
-                  ZONESDIR
+usage: nsdgenzone.py [-h] --zones ZONES --domainname DOMAINNAME --ipaddress \
+                     IPADDRESS --zonesdir ZONESDIR --configfile CONFIGFILE \
+                     --queryfile QUERYFILE
 
 options:
   -h, --help            show this help message and exit
   --zones ZONES         Number of zones.
-  --hosts HOSTS         Number of hosts.
   --domainname DOMAINNAME
-                        Base of domain name.
+                        Domain name.
   --ipaddress IPADDRESS
                         IP address of name server.
-  --configdir CONFIGDIR
-                        Config directory of NSD.
   --zonesdir ZONESDIR   Directory of zones directory.
+  --configfile CONFIGFILE
+                        The configuration file for the NSD zone clause to be created.
+  --queryfile QUERYFILE
+                        The data file to be created, used for dnsperf queries.
 ```
 
 For example,
 
 ```
-nsdgenzone --zones 1000000 --hosts 10 --domainname test. \
-    --ipaddress 192.0.2.53 --configdir /etc/nsd/nsd.conf.d \
-    --zonesdir /etc/nsd/zones 
+nsdgenzone --zones 1000000 --domainname test. --ipaddress 192.0.2.53 \
+    --zonesdir /etc/nsd/zones --configfile /etc/nsd/nsd.conf.d/zones.conf \
+    --queryfile querydata.txt
 ```
 
--- nsdgenzone.py
+++ nsdgenzone.py
@@ -14,47 +14,55 @@ if TYPE_CHECKING:
 MAX_FILE_IN_DIR = 100
 DEFAULT_TTL = 300
 SOA_MINIMUM = 300
-CONFIG_FILE = "zones.conf"
 ZONEFILE_SUFFIX = "zone"
 
 
-def generate_zonefile(
-    zonefile: Path, zonename: str, ipaddress: str, hosts: int
-) -> None:
+def generate_zonefile(zonefile: Path, zonename: str, ipaddress: str) -> None:
     """Generate a zone file."""
     lines: list[str] = []
     lines.append(f"$TTL {DEFAULT_TTL}")
     lines.append(
         f"{zonename} IN SOA ns.{zonename} hostmaster.{zonename} "
-        f"2023100702 3600 900 604800 {SOA_MINIMUM}"
+        f"2023100701 3600 900 604800 {SOA_MINIMUM}"
     )
-    lines.append(f"@  IN NS ns.{zonename}")
-    lines.append(f"ns IN A {ipaddress}")
-    hostname_digit: int = ceil(log10(hosts))
-    for i in range(0, hosts):
-        lines.append(f"{i:0{hostname_digit}} IN A {ipaddress}")
+    lines.append(f"@   IN  NS    ns.{zonename}")
+    lines.append(f"ns  IN  A     {ipaddress}")
+    lines.append(f'@   IN  TXT   "v=spf1 ip4:{ipaddress} -all"')
+    lines.append(f"@   IN  A     {ipaddress}")
+    lines.append(f"www IN  CNAME {zonename}")
+    lines.append(f"@   IN  MX    mx.{zonename}")
+    lines.append(f"mx  IN  A     {ipaddress}")
     lines.append("")
-
     zonefile.write_text("\n".join(lines))
 
 
-def generate_zone_config(zonename: str, zonefile: Path) -> str:
-    """Generate zone configuration."""
+def generate_querylines(zonename: str) -> list[str]:
+    """Generate query data lines."""
+    lines: list[str] = []
+    lines.append(f"{zonename} A")
+    lines.append(f"{zonename} TXT")
+    lines.append(f"www.{zonename} A")
+    lines.append(f"{zonename} MX")
+    return lines
+
+
+def generate_configlines(zonename: str, zonefile: Path) -> list[str]:
+    """Generate configuration lines."""
     lines: list[str] = []
     lines.append("zone:")
     lines.append(f"        name: {zonename}")
     lines.append(f"        zonefile: {zonefile}")
     lines.append("")
-    return "\n".join(lines)
+    return lines
 
 
 def generate(
     zones: int,
-    hosts: int,
     domainname: str,
     ipaddress: str,
-    configdir: Path,
     zonesdir: Path,
+    configfile: Path,
+    queryfile: Path,
 ) -> None:
     """Generate zone files and a config file."""
     if not domainname.endswith("."):
@@ -65,6 +73,7 @@ def generate(
     depth: int = floor((subdomainname_digit - 1) / 2)
 
     configlines: list[str] = []
+    querylines: list[str] = []
 
     for i in range(0, zones):
         zonename: str = f"{i:0{subdomainname_digit}}.{domainname}"
@@ -78,13 +87,15 @@ def generate(
             dirname.mkdir(parents=True, exist_ok=True)
 
         zonefile: Path = dirname.joinpath(f"{zonename}{ZONEFILE_SUFFIX}")
-        generate_zonefile(zonefile, zonename, ipaddress, hosts)
+        generate_zonefile(zonefile, zonename, ipaddress)
 
-        zoneconfig: str = generate_zone_config(zonename, zonefile)
-        configlines.append(zoneconfig)
+        configlines.extend(generate_configlines(zonename, zonefile))
+        querylines.extend(generate_querylines(zonename))
 
-    configfile: Path = configdir.joinpath(CONFIG_FILE)
     configfile.write_text("\n".join(configlines))
+
+    querylines.append("")
+    queryfile.write_text("\n".join(querylines))
 
 
 def main() -> int:
@@ -99,20 +110,13 @@ def main() -> int:
         help="Number of zones.",
     )
     parser.add_argument(
-        "--hosts",
-        dest="hosts",
-        action="store",
-        type=int,
-        required=True,
-        help="Number of hosts.",
-    )
-    parser.add_argument(
         "--domainname",
         dest="domainname",
         action="store",
         type=str,
         required=True,
-        help="Base of domain name.",
+        default="test.",
+        help="Domain name.",
     )
     parser.add_argument(
         "--ipaddress",
@@ -123,34 +127,45 @@ def main() -> int:
         help="IP address of name server.",
     )
     parser.add_argument(
-        "--configdir",
-        dest="configdir",
-        action="store",
-        type=Path,
-        required=True,
-        help="Config directory of NSD.",
-    )
-    parser.add_argument(
         "--zonesdir",
         dest="zonesdir",
         action="store",
         type=Path,
         required=True,
-        help="Directory of zones directory.",
+        help="Zones directory.",
+    )
+    parser.add_argument(
+        "--configfile",
+        dest="configfile",
+        action="store",
+        type=Path,
+        default=Path("/etc/nsd/nsd.conf.d/zones.conf"),
+        required=True,
+        help="Configuration file for NSD zone clauses to be created.",
+    )
+    parser.add_argument(
+        "--queryfile",
+        dest="queryfile",
+        action="store",
+        type=Path,
+        required=True,
+        help="Data file to be created, used for dnsperf queries.",
     )
     args: Namespace = parser.parse_args()
-    if not args.configdir.is_dir():
-        parser.error(f"argument --configdir: no such directory: '{args.configdir}'")
     if not args.zonesdir.is_dir():
         parser.error(f"argument --zonesdir: no such directory: '{args.zonesdir}'")
+    if args.configfile.is_file():
+        parser.error(f"argument --configfile: already exists: '{args.configfile}'")
+    if args.queryfile.is_file():
+        parser.error(f"argument --queryfile: already exists: '{args.queryfile}'")
 
     generate(
         zones=args.zones,
-        hosts=args.hosts,
         domainname=args.domainname,
         ipaddress=args.ipaddress,
-        configdir=args.configdir,
         zonesdir=args.zonesdir,
+        configfile=args.configfile,
+        queryfile=args.queryfile,
     )
     return 0
 
